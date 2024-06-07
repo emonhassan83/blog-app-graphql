5d6bf6c146b46a13f942715a769f6f7438e9c2f1
Emon <emonhasan7650@gmail.com>
2024-06-07 12:12:00 +0600

    prisma schema created and graphql schema added

---
+++ src/schema.ts
@@ -0,0 +1,31 @@
+export const typeDefs = `#graphql
+    type Query {
+        me: User
+        posts: [Post]
+    }
+
+  type Post {
+    id: ID!
+    title: String!
+    content: String!
+    author: User
+    cratedAt: String!
+    updatedAt: String!
+    published: Boolean!
+  }
+
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+    createdAt: String!
+    posts: [Post]
+  }
+
+   type Profile {
+        id: ID!
+        bio: String!
+        createdAt: String!
+        user: User!
+    }
+`;
