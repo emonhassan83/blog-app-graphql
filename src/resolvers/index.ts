85254214076c2cdb233ded5a6260b355810d3059
Emon <emonhasan7650@gmail.com>
2024-06-22 15:50:04 +0600

    post, user, profile relation apply done

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,7 +1,13 @@
+import { Post } from "../post";
+import { Profile } from "../profile";
+import { User } from "../user";
 import { Mutation } from "./Mutation/Mutation";
 import { Query } from "./Query/Query";
 
 export const resolvers = {
   Query,
+  Post,
+  User,
+  Profile,
   Mutation,
 };
